32d2e12270dc29170a134891ab0b4d371897c370
Pablo Ventura <pablogventura@gmail.com>
2016-09-21 18:14:17 -0300

    bajo aceleracion

--- firmware_marlin/Marlin_print_3D_FINAL/Configuration.h
+++ firmware_marlin/Marlin_print_3D_FINAL/Configuration.h
@@ -289,7 +289,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {160,160,8000,293}  // default steps per unit for ultimaker
 #define DEFAULT_MAX_FEEDRATE          {330, 330, 3.3, 45}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {1500,1500,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_ACCELERATION      {1000,1000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
                                     //(9000,9000,100,10000)
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves [3000]
 #define DEFAULT_RETRACT_ACCELERATION  1000   // X, Y, Z and E max acceleration in mm/s^2 for r retracts [3000]
