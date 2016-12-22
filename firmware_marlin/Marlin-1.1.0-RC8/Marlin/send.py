095c9f89fda490b95a94e3bc79890980924fa471
Pablo Ventura <pablogventura@gmail.com>
2016-12-22 12:57:04 -0300

    ahora si

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
@@ -7,20 +7,11 @@ import StringIO
 comandos="sudo service octoprint stop; export DISPLAY=:0;cd miprusa/firmware_marlin/Marlin-1.1.0-RC8/Marlin/;git pull;/home/linaro/arduino-1.6.12/arduino --upload Marlin.ino;sudo service octoprint start; echo listo"
 output = StringIO.StringIO()
 
-c=pexpect.spawn("ssh linaro@192.168.1.137 " + comandos, timeout=200)
+c=pexpect.spawn("ssh linaro@192.168.1.137 " + comandos, timeout=30)
 c.logfile=output
-c.sendline("linaro")
-c.expect(["done"])
-print "Octoprint detenido"
-if c.expect(["Updating", "up-to-date"]) == 0:
-    print "Actualiando repo"
-else:
-    print "El repo esta al dia"
-    
-c.expect(["done"])
-print "Octoprint inciado"
-
 assert c.expect(["password: "]) == 0
+c.sendline("linaro")
+
 
 c.expect(["listo"])
 
