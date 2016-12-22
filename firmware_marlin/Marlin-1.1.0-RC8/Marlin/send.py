9080d35e7114d3f46cef866341aa176ef5ba979b
Pablo Ventura <pablogventura@gmail.com>
2016-12-22 12:42:46 -0300

    script para mandar el codigo

---
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import pexpect
+import StringIO
+
+comandos="sudo service octoprint stop; export DISPLAY=:0;cd miprusa/firmware_marlin/Marlin-1.1.0-RC8/Marlin/;git pull;/home/linaro/arduino-1.6.12/arduino --upload Marlin.ino;sudo service octoprint start; echo listo"
+output = StringIO.StringIO()
+
+c=pexpect.spawn("ssh linaro@192.168.1.137 " + comandos, timeout=200)
+
+c.logfile=output
+
+assert c.expect(["password: "]) == 0
+
+c.sendline("linaro")
+c.expect(["listo"])
+
+
+print(output.getvalue())
