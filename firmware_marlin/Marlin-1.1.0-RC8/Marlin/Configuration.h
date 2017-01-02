2cbfe64f84c36612bcd59096b591f4bcc8d249f9
Pablo Ventura <pablogventura@gmail.com>
2017-01-02 04:40:42 -0300

    corrigiendo altura

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
@@ -584,7 +584,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -26   // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -28   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.5  // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.9  // Z offset: -below +above  [the nozzle] //restar aca, acerca el extrusor a la cama
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 10000
