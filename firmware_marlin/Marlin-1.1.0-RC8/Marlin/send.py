0ff4cebe810516899f56ebee4182c4038deefc3f
Pablo Ventura <pablogventura@gmail.com>
2017-02-08 14:59:03 -0300

    mejora send.py

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
@@ -4,7 +4,13 @@
 import pexpect
 import StringIO
 
-comandos="sudo service octoprint stop; export DISPLAY=:0;cd miprusa/firmware_marlin/Marlin-1.1.0-RC8/Marlin/;git pull;/home/linaro/arduino-1.8.0/arduino --upload Marlin.ino;sudo service octoprint start; echo listo"
+comandos = "sudo service octoprint stop; "
+comandos+= "export DISPLAY=:0; "
+comandos+= "cd arduino-1.8.0; "
+comandos+= "git pull; "
+comandos+= "./arduino --upload ../miprusa/firmware_marlin/Marlin-1.1.0-RC8/Marlin/Marlin.ino; "
+comandos+= "sudo service octoprint start; "
+comandos+= "echo listo"
 output = StringIO.StringIO()
 
 c=pexpect.spawn("ssh linaro@192.168.1.137 " + comandos, timeout=200)
