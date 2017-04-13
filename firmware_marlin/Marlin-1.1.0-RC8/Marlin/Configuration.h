d3c8d0dc5c43d6be994637431417ed5b13c2109a
Pablo Ventura <pablogventura@gmail.com>
2017-04-12 22:24:52 -0300

    aumento cantidad de pasos del extrusor

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
@@ -490,7 +490,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,145}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,150}
 
 /**
  * Default Max Feed Rate (mm/s)
