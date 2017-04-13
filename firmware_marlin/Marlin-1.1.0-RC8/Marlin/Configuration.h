fea5e5d8f3d44eb289df721a0bd98ac6112b7493
Pablo Ventura <pablogventura@gmail.com>
2017-04-13 10:17:30 -0300

    152 pasos en el extrusor

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
@@ -490,7 +490,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,150}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,152}
 
 /**
  * Default Max Feed Rate (mm/s)
