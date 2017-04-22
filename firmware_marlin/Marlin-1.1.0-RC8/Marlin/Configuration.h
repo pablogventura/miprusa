4ad36f33c1cf0cb0e0b039c0387429fac0d1a27a
Pablo Ventura <pablogventura@gmail.com>
2017-04-21 23:59:18 -0300

    autopid bed

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/Configuration.h
@@ -362,10 +362,12 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
-
+  ////#define  DEFAULT_bedKp 10.00 valores originales
+  ////#define  DEFAULT_bedKi .023
+  ////#define  DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 202.96 //valores con el autopid
+  #define DEFAULT_bedKi 37.54
+  #define DEFAULT_bedKd 274.32
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
   //#define  DEFAULT_bedKp 97.1
