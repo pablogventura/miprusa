4db5c79787757e513638919fb65208b2a70cccab
Pablo Ventura <pablogventura@gmail.com>
2017-05-07 16:55:41 -0300

    speaker

--- firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
+++ firmware_marlin/Marlin-1.1.0-RC8/Marlin/send.py
@@ -14,7 +14,7 @@ comandos+= "sudo service octoprint start; "
 comandos+= "echo listo"
 output = StringIO.StringIO()
 
-c=pexpect.spawn("ssh pablo@192.168.2.137 " + comandos, timeout=200)
+c=pexpect.spawn("ssh pablo@192.168.2.137 " + comandos, timeout=400)
 c.logfile=output
 assert c.expect(["password: "]) == 0
 c.sendline(getpass("contraseña: "))
