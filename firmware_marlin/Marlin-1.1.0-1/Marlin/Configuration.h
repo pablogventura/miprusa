4b4e6c55e7499dc166537a6094302cb6eb4d184e
Pablo Ventura <pablogventura@gmail.com>
2017-05-17 11:32:46 -0300

    corrijo front probe

--- firmware_marlin/Marlin-1.1.0-1/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-1/Marlin/Configuration.h
@@ -833,7 +833,7 @@
   // Set the boundaries for probing (where the probe can reach).
   #define LEFT_PROBE_BED_POSITION (X_MIN_POS + 30)
   #define RIGHT_PROBE_BED_POSITION (X_MAX_POS - 30)
-  #define FRONT_PROBE_BED_POSITION (Y_MIN_POS + 60)
+  #define FRONT_PROBE_BED_POSITION (Y_MIN_POS + 61)
   #define BACK_PROBE_BED_POSITION (Y_MAX_POS - 30)
 
   // The Z probe minimum outer margin (to validate G29 parameters).
