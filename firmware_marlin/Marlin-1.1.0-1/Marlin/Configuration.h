a8b5f8c068484cc9f4896b89410d5cde8abf0de4
Pablo Ventura <pablogventura@gmail.com>
2017-05-18 16:51:02 -0300

    vuelvo a 1/16 micropasos

--- firmware_marlin/Marlin-1.1.0-1/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-1/Marlin/Configuration.h
@@ -486,7 +486,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 400, 158 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 158 }
 
 /**
  * Default Max Feed Rate (mm/s)
