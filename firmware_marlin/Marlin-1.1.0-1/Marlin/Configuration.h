1d212ad4d61853361f1fa36b39ccacb8515d7db8
Pablo Ventura <pablogventura@gmail.com>
2017-05-19 14:58:55 -0300

    nozzle autoclean

--- firmware_marlin/Marlin-1.1.0-1/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.0-1/Marlin/Configuration.h
@@ -1062,18 +1062,18 @@
  * Attention: EXPERIMENTAL. G-code arguments may change.
  *
  */
-//#define NOZZLE_CLEAN_FEATURE
+#define NOZZLE_CLEAN_FEATURE
 
 #if ENABLED(NOZZLE_CLEAN_FEATURE)
   // Default number of pattern repetitions
-  #define NOZZLE_CLEAN_STROKES  12
+  #define NOZZLE_CLEAN_STROKES  2
 
   // Default number of triangles
   #define NOZZLE_CLEAN_TRIANGLES  3
 
   // Specify positions as { X, Y, Z }
-  #define NOZZLE_CLEAN_START_POINT { 30, 30, (Z_MIN_POS + 1)}
-  #define NOZZLE_CLEAN_END_POINT   {100, 60, (Z_MIN_POS + 1)}
+  #define NOZZLE_CLEAN_START_POINT { -10, 35, 8}
+  #define NOZZLE_CLEAN_END_POINT   {-10, 85, 8}
 
   // Circular pattern radius
   #define NOZZLE_CLEAN_CIRCLE_RADIUS 6.5
