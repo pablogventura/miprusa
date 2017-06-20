8b67743c851f49b74a84bb6f5851e335952d3f5c
Pablo Ventura <pablogventura@gmail.com>
2017-06-19 21:42:35 -0300

    acomodando anchos

--- firmware_marlin/Marlin-1.1.3/Marlin/Configuration.h
+++ firmware_marlin/Marlin-1.1.3/Marlin/Configuration.h
@@ -499,7 +499,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 158 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 94, 95, 400, 158 } // antes 100, 100, 400, 158 }
 
 /**
  * Default Max Feed Rate (mm/s)
