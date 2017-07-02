5ff8069ad4687e2dfc16c0d0e89bc686d2c05d5d
Pablo Ventura <pablogventura@gmail.com>
2017-07-02 10:28:21 -0300

    send a raspberry

--- firmware_marlin/send.py
+++ firmware_marlin/send.py
@@ -8,13 +8,13 @@ comandos = "cd miprusa/;"
 comandos+= "git pull; "
 comandos+= "cd; "
 comandos+= "sudo service octoprint stop; "
-comandos+= "cd arduino-1.8.1; "
+comandos+= "cd arduino-1.8.3; "
 comandos+= "./arduino --upload ../miprusa/firmware_marlin/Marlin-1.1.3/Marlin/Marlin.ino; "
 comandos+= "sudo service octoprint start; "
 comandos+= "echo listo"
 output = StringIO.StringIO()
 
-c=pexpect.spawn("ssh pablo@192.168.2.137 " + comandos, timeout=400)
+c=pexpect.spawn("ssh pi@192.168.2.186 " + comandos, timeout=400)
 c.logfile=output
 assert c.expect(["password: "]) == 0
 c.sendline(getpass("contraseña: "))
