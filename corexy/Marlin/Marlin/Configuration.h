c500f2b5698f9d737eeaed65c18be398bd6687df
Pablo Ventura <pablo@claude.workgroup>
2017-09-06 20:30:26 -0300

    centrando

--- corexy/Marlin/Marlin/Configuration.h
+++ corexy/Marlin/Marlin/Configuration.h
@@ -766,8 +766,8 @@
 // @section machine
 
 // Travel limits after homing (units are in mm)
-#define X_MIN_POS 0
-#define Y_MIN_POS -55
+#define X_MIN_POS 7
+#define Y_MIN_POS -33
 #define Z_MIN_POS 0
 #define X_MAX_POS 300
 #define Y_MAX_POS 170
