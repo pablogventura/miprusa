31008752a3508b5efaab90322f352444076e2cea
Pablo Ventura <pablo@claude.workgroup>
2018-01-15 01:19:56 -0300

    bed max temp

--- corexy/Marlin/Marlin/Configuration.h
+++ corexy/Marlin/Marlin/Configuration.h
@@ -327,7 +327,7 @@
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
-#define BED_MAXTEMP 150
+#define BED_MAXTEMP 200
 
 //===========================================================================
 //============================= PID Settings ================================
