ce82e7168415b71c940b6fed60fcaef2bd0d4c27
Pablo Ventura <pablo@claude.workgroup>
2018-09-03 16:28:07 -0300

    pasos

--- corexy/Marlin/Marlin/Configuration.h
+++ corexy/Marlin/Marlin/Configuration.h
@@ -601,7 +601,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 315 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 375 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -771,7 +771,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 0  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 60  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.5   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.6   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
