55d32c1832e891e62551ece34ffc69c4b68ed2dd
Pablo Ventura <pablo@claude.WORKGROUP>
2019-01-03 15:49:04 -0300

    actualizo

--- corexy/Marlin/Marlin/Configuration.h
+++ corexy/Marlin/Marlin/Configuration.h
@@ -770,7 +770,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 0  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 60  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.6   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.9   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
