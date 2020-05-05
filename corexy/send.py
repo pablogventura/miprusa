dac7104e89948206e4e024f9ed93b17cbb6e8d77
Pablo <pablogventura@gmail.com>
2020-05-05 11:14:46 -0300

    mejora send

--- corexy/send.py
+++ corexy/send.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 
 import pexpect
-import StringIO
+from io import BytesIO
 from getpass import getpass
 comandos = "cd miprusa/;"
 comandos+= "git pull; "
@@ -12,13 +12,13 @@ comandos+= "cd arduino-1.8.5; "
 comandos+= "./arduino --upload ../miprusa/corexy/Marlin/Marlin/Marlin.ino; "
 comandos+= "sudo service octoprint start; "
 comandos+= "echo listo"
-output = StringIO.StringIO()
+output = BytesIO()
 
 c=pexpect.spawn("ssh pi@impresora3d " + comandos, timeout=60*10)
 c.logfile=output
 assert c.expect(["password: "]) == 0
 c.sendline(getpass("contraseña: "))
-print output.getvalue()
+print(output.getvalue())
 
 c.expect(["listo"])
 
